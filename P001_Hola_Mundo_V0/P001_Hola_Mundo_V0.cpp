060932e9c487a22adcf8bfc9e93bb63d27cab688
Macabarellano <macabarellano@gmail.com>
2023-09-11 07:23:40 -0600

    000_Mau
    Primer algoritmo

---
+++ P001_Hola_Mundo_V0/P001_Hola_Mundo_V0.cpp
@@ -0,0 +1,17 @@
+// P001_Hola_Mundo_V0.cpp : 
+// Mauricio Cabrera
+// En este programa nos comunicaremos con el usuario
+// mediante la terminal
+
+// Librerias!
+#include <iostream>
+
+//Función inicial
+int main()
+{
+    //Sintaxys para hablar con el usuario
+    // \n nos sirve para hacer un salto de linea
+    //Diagonal invertida alt+92
+    std::cout << "Hello stranger\n";
+    std::cout << std::endl;
+}
